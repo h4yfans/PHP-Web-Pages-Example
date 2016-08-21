19ba9dc2d9ada78e2f72dbd16f384afecd78183d
Kaan Karaca <kaan94karaca@gmail.com>
2016-08-21 22:42:15 +0300

    PHP Lovers Blog

--- Project 3 - PHP Lovers Blog/index.php
+++ Project 3 - PHP Lovers Blog/index.php
@@ -1,26 +1,30 @@
-<?php include 'config/config.php' ?>
-<?php include 'libraries/Database.php' ?>
 <?php include 'includes/header.php' ?>
-<?php include 'helpers/format_helper.php' ?>
+
 <?php
 //Create DB Object
-$db = new Database();
+$db = new Database;
 
 //Create Query
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts ORDER BY id DESC";
 
 //Run Query
 $posts = $db->select($query);
+
+
+//Create Query
+$query = "SELECT * FROM categories";
+
+//Run Query
+$categories = $db->select($query);
+
 ?>
 <?php if ($posts): ?>
-    <?php while($row = $posts->fetch_assoc()): ?>
-
+    <?php while($row = $posts->fetch_assoc()):?>
     <div class="blog-post">
         <h2 class="blog-post-title"><?php echo $row['title']; ?></h2>
         <p class="blog-post-meta"> <?php echo formatDate($row['date']); ?> by <a href="#"><?php echo $row['author']; ?></a> </p>
         <?php echo shorterText($row['body']) ;?>
         <a class="readmore" href="post.php?id=<?php echo urlencode($row['id']); ?>">Read More</a>
-
     </div>
 
 <?php endwhile; ?>
